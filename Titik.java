70a5595fd0aae869531f7ca2bae1791ac801a63d
Abi Satya Deville <118191553+bisat19@users.noreply.github.com>
2024-02-20 14:13:13 +0700

    Add files via upload

---
+++ Titik.java
@@ -0,0 +1,42 @@
+// Abisatya Hastarangga Pradana - 24060122120004
+/**
+ * Titik
+ */
+public class Titik {
+    double absis;
+    double ordinat;
+    static double counterTitik;
+
+    //konstruktor
+    Titik(){
+        absis = 0;
+        ordinat = 0;
+        counterTitik ++;
+    }
+    
+    Titik(double absis, double ordinat){
+        this.absis = absis;
+        this.ordinat = ordinat;
+    }
+    
+    double getAbsis(){
+        return this.absis;
+    }
+
+    double getOrdinat(){
+        return this.ordinat;
+    }
+
+    static double getCounterTitik(){
+        return counterTitik;
+    }
+
+    void setAbsis(double absis){
+        this.absis = absis;
+    }
+
+    void setOrdinat(double ordinat){
+        this.ordinat = ordinat;
+    }
+
+}
