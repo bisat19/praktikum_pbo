8634b87782bed525dd2c1e0bfd2b177b841d86d5
Abi Satya Deville <118191553+bisat19@users.noreply.github.com>
2024-02-21 11:56:50 +0700

    Add files via upload

---
+++ praktikum1/Titik.java
@@ -0,0 +1,43 @@
+//Nama :Abisatya Hastarangga Pradana
+//NIM  :24060122120004
+//Lab C1
+/** Titik **/
+public class Titik {
+    double absis;
+    double ordinat;
+    static double counterTitik;
+
+    //konstruktor
+    Titik(){
+        absis = 0;
+        ordinat = 0;
+        counterTitik ++;
+    }
+    
+    Titik(double absis, double ordinat){
+        this.absis = absis;
+        this.ordinat = ordinat;
+        counterTitik ++;
+    }
+    
+    double getAbsis(){
+        return this.absis;
+    }
+
+    double getOrdinat(){
+        return this.ordinat;
+    }
+
+    static double getCounterTitik(){
+        return counterTitik;
+    }
+
+    void setAbsis(double absis){
+        this.absis = absis;
+    }
+
+    void setOrdinat(double ordinat){
+        this.ordinat = ordinat;
+    }
+
+}
